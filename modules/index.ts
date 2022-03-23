c572d8fa61648c471f655ecee443852fb96b74a4
Onurhann Aytac <onurhan.aytac@obss.com.tr>
2022-03-23 17:55:02 +0300

    auth mw import

--- modules/index.ts
+++ modules/index.ts
@@ -2,7 +2,7 @@ import { Express } from "express";
 import { UsersController } from "./users";
 import { ContactsController } from "./contacts";
 import { LoginController } from "./login";
-import auth from "../middlewares/auth.middleware";
+import { auth } from "../middlewares";
 
 const routes = (app: Express): void => {
   app.use("/api/v1", auth, UsersController);
